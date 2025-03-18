be22016eec71a92fc66d6914281c901f78012d26
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-03-18 16:20:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function onesDigit(n) {
   // your code here
+	 if (!Number.isInteger(n) || n<= 0) {
+        throw new Error("Input must be a positive integer");
+    }
+    return n % 10;
+
 }
 
 // Do not change the code below
